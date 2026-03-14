d789f16497dc31e443a1f83926f2fd6e89e0c604
wanidanish66 <153353869+wanidanish66@users.noreply.github.com>
2026-03-15 01:12:08 +0530

    installed some packages on the virtual environment

---
+++ main.py
@@ -0,0 +1,3 @@
+import speech_recognition as sr     # This allows you to reference the speech_recognition library using the shorter name sr throughout your code
+import webbrowser
+if __name__ == "__main__":
