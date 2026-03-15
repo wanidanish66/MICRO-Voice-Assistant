243823dd8259d8269da2f7921ad567d9d4eb74c9
wanidanish66 <153353869+wanidanish66@users.noreply.github.com>
2026-03-15 15:35:10 +0530

    used recognizer class for speech recognition functionality and updated code which getes activated with the wake word micro

--- main.py
+++ main.py
@@ -1,3 +1,45 @@
 import speech_recognition as sr     # This allows you to reference the speech_recognition library using the shorter name sr throughout your code
 import webbrowser
+import pyttsx3
+
+r = sr.Recognizer()        # recognizer class helps you to take speech recognition functionality
+engine = pyttsx3.init()             # initilizing pyttsx
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def processCommand(command):
+    print("Command:", command)
+
+
 if __name__ == "__main__":
+    speak("Intializing Micro......")
+    while True:
+        # listen for the wake word "Micro"
+        # Use the default microphone as the audio source
+    
+        print("recognizing....")
+        # recognize speech using bing
+        try:
+            with sr.Microphone() as source:
+                r.adjust_for_ambient_noise(source)
+                print("Listening....")
+                audio = r.listen(source, phrase_time_limit=4)
+
+            word = r.recognize_google(audio).lower()
+            print("Heard", word)
+
+            if "micro" in word.lower():
+                speak("Ya")
+                # listen for command
+                with sr.Microphone() as source:
+                    print("Micro active....")
+                    audio = r.listen(source, phrase_time_limit=4)
+
+                command = r.recognize_google(audio)
+                processCommand(command)
+
+        except Exception as e:
+            print("Error: ",e)
+       
