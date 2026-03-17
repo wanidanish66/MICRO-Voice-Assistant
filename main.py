197e298d95f3ce100a3a5b286bb64cf4c4ef8a6b
wanidanish66 <153353869+wanidanish66@users.noreply.github.com>
2026-03-18 00:07:57 +0530

    updated code to open songs by voice command

--- main.py
+++ main.py
@@ -1,47 +1,61 @@
 import speech_recognition as sr     # This allows you to reference the speech_recognition library using the shorter name sr throughout your code
 import webbrowser
 import pyttsx3
-
-r = sr.Recognizer()        # recognizer class helps you to take speech recognition functionality
+import musicLibrary
+import requests
+recognizer = sr.Recognizer()        # recognizer class helps you to take speech recognition functionality
 engine = pyttsx3.init()             # initilizing pyttsx
+newsapi = "b258ed3179e8414f94df2743db0b0ef2"
 
-PHRASE_TIME_LIMIT = 5  # Set the phrase time limit in seconds
- 
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-def processCommand(command):
-    print("Command:", command)
+def processCommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://google.com")
+    elif "open facebook" in c.lower():
+        webbrowser.open("https://facebook.com")
+    elif "open facebook" in c.lower():
+        webbrowser.open("https://instagram.com")
+    elif "open facebook" in c.lower():
+        webbrowser.open("https://linkdin.com")
+    elif "open facebook" in c.lower():
+        webbrowser.open("https://github.com")
+    elif "open facebook" in c.lower():
+        webbrowser.open("https://youtube.com")
 
+    # for music library
+    elif c.lower().startswith("play"):
+        song = c.lower().split(" ")[1]
+        link = musicLibrary.music[song]
+        webbrowser.open(link)
 
 if __name__ == "__main__":
     speak("Intializing Micro......")
     while True:
         # listen for the wake word "Micro"
         # Use the default microphone as the audio source
-    
+        r = sr.Recognizer()
+        
+        
         print("recognizing....")
         # recognize speech using bing
         try:
-            with sr.Microphone() as source:
-                r.adjust_for_ambient_noise(source)
+            with sr.Microphone(device_index=2) as source:
                 print("Listening....")
-                audio = r.listen(source, phrase_time_limit=PHRASE_TIME_LIMIT)
+                audio = r.listen(source, timeout=2, phrase_time_limit=3)
+            word = r.recognize_google(audio)
 
-            word = r.recognize_google(audio).lower()
-            print("Heard:", word)
+            if "hello micro" in word.lower():
+                speak("hello sir")
 
-            if "micro" in word:
-                speak("Yes")
                 # listen for command
-                with sr.Microphone() as source:
+                with sr.Microphone(device_index=2) as source:
                     print("Micro active....")
-                    audio = r.listen(source, phrase_time_limit=PHRASE_TIME_LIMIT)
-
-                command = r.recognize_google(audio)
-                processCommand(command)
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+                    processCommand(command)
 
         except Exception as e:
-            print("Error: ",e)
-       
+            print("Error:", e)
---
+++ musicLibrary.py
@@ -0,0 +1,3 @@
+music = {
+    "headlight": "https://www.youtube.com/watch?v=IJZ8HKn6OZA&list=RDIJZ8HKn6OZA&start_radio=1&pp=oAcB"    
+}  
